3b3c4ba9c07729370e111bd7b888ba5ed4310088
Martín M <skydiver@users.noreply.github.com>
2020-02-12 22:29:10 -0300

    plugin enhancements

--- controllers/Monitor.php
+++ controllers/Monitor.php
@@ -1,25 +1,35 @@
 <?php
 
-    namespace Martin\MonitorClient\Controllers;
+namespace Martin\MonitorClient\Controllers;
 
-    use App, Response;
-    use Martin\MonitorClient\Classes\Updates;
-    use Martin\MonitorClient\Models\Settings;
+use App, Config, Response;
+use Martin\MonitorClient\Classes\Updates;
+use Martin\MonitorClient\Models\Settings;
 
-    class Monitor extends \Backend\Classes\Controller {
+class Monitor extends \Backend\Classes\Controller {
 
-        public $publicActions = ['updates'];
+    public $publicActions = ['updates'];
 
-        public function updates($token=null) {
+    public function updates($token=null) {
 
-            if($token == '' OR $token != Settings::get('token')) {
-                App::abort(404, '404 Not Found');
-            }
-
-            $updates = Updates::available();
-            return \Response::json($updates);
-
+        if ($token === '' OR $token !== Settings::get('token')) {
+            App::abort(404, '404 Not Found');
         }
+
+        $updates = Updates::available();
+
+        if (Settings::get('enable_encryption')) {
+            $cipher = Config::get('app.cipher');
+            $key = \Str::replaceFirst('base64:', '', Config::get('app.key'));
+            $key = base64_decode($key);
+            $newEncrypter = new \Illuminate\Encryption\Encrypter($key, $cipher);
+            $updates = $newEncrypter->encrypt($updates);
+        }
+
+        return Response::json($updates);
+
     }
+
+}
 
 ?>
