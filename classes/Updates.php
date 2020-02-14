5f8735d14e557e244dc490ff06ac2f8db4f19e06
Martín M <skydiver@users.noreply.github.com>
2020-02-14 00:41:44 -0300

    linting

--- classes/Updates.php
+++ classes/Updates.php
@@ -1,40 +1,40 @@
 <?php
 
-    namespace Martin\MonitorClient\Classes;
+namespace Martin\MonitorClient\Classes;
 
-    use Illuminate\Http\Response;
-    use System\Classes\UpdateManager;
+use Illuminate\Http\Response;
+use System\Classes\UpdateManager;
 
-    class Updates extends Response {
+class Updates extends Response {
 
-        public static function available() {
+    public static function available() {
 
-            $manager = UpdateManager::instance();
-            $updates = $manager->requestUpdateList(true);
-            $return  = ['core' => [], 'plugins' => []];
+        $manager = UpdateManager::instance();
+        $updates = $manager->requestUpdateList(true);
+        $return  = ['core' => [], 'plugins' => []];
 
-            if(isset($updates['core'])) {
-                $return['core'] = [
-                    'old' => $updates['core']['old_build'],
-                    'new' => $updates['core']['build']
-                ];
-            }
-
-            if(isset($updates['plugins']) && count($updates['plugins']) > 0) {
-                foreach($updates['plugins'] as $name => $plugin) {
-                    $return['plugins'][] = [
-                        'name'      => $plugin['name'],
-                        'namespace' => $name,
-                        'old'       => $plugin['old_version'],
-                        'new'       => $plugin['version']
-                    ];
-                }
-            }
-
-            return $return;
-
+        if (isset($updates['core'])) {
+            $return['core'] = [
+                'old' => $updates['core']['old_build'],
+                'new' => $updates['core']['build']
+            ];
         }
 
+        if (isset($updates['plugins']) && count($updates['plugins']) > 0) {
+            foreach ($updates['plugins'] as $name => $plugin) {
+                $return['plugins'][] = [
+                    'name'      => $plugin['name'],
+                    'namespace' => $name,
+                    'old'       => $plugin['old_version'],
+                    'new'       => $plugin['version']
+                ];
+            }
+        }
+
+        return $return;
+
     }
+
+}
 
 ?>
