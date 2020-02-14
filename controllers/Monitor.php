283452a49f0f81521dcc26dc469426cea51d1b5b
Martín M <skydiver@users.noreply.github.com>
2020-02-14 00:55:10 -0300

    change url, code refactor

--- controllers/Monitor.php
+++ controllers/Monitor.php
@@ -5,12 +5,13 @@ namespace Martin\MonitorClient\Controllers;
 use App, Config, Response;
 use Martin\MonitorClient\Classes\Updates;
 use Martin\MonitorClient\Models\Settings;
+use Illuminate\Encryption\Encrypter;
 
 class Monitor extends \Backend\Classes\Controller {
 
-    public $publicActions = ['updates'];
+    public $publicActions = ['status'];
 
-    public function updates($token=null) {
+    public function status($token=null) {
 
         if ($token === '' OR $token !== Settings::get('token')) {
             App::abort(404, '404 Not Found');
@@ -18,15 +19,19 @@ class Monitor extends \Backend\Classes\Controller {
 
         $updates = Updates::available();
 
-        if (Settings::get('enable_encryption')) {
-            $cipher = Config::get('app.cipher');
-            $key = \Str::replaceFirst('base64:', '', Config::get('app.key'));
-            $key = base64_decode($key);
-            $newEncrypter = new \Illuminate\Encryption\Encrypter($key, $cipher);
-            $updates = $newEncrypter->encrypt($updates);
+        $data = array_merge([], $updates);
+
+        if (!Settings::get('enable_encryption')) {
+            return Response::json($data);
         }
 
-        return Response::json($updates);
+        // return encrypted data
+        $cipher = Config::get('app.cipher');
+        $key = \Str::replaceFirst('base64:', '', Config::get('app.key'));
+        $key = base64_decode($key);
+        $newEncrypter = new Encrypter($key, $cipher);
+        $data = $newEncrypter->encrypt($data);
+        return Response::json($data);
 
     }
 
